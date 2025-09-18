1a54f83147351bd1338b6ddea00687237c453485
Artandrey <artandrey7777@gmail.com>
2025-09-18 22:36:07 +0300

    feat: add CLI entry point

---
+++ bin/index.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+const { cliController } = require('../dist/index.cjs');
+cliController.bootstrap();
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,8 @@ import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { files: ['**/*.{mjs,cjs,ts}'] },
+  { ignores: ['src/bin/**/*.js'] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
